2b90585ccf91e2396c7b26624915fee6439eeab4
Felipe Gardin <gardinfelipe@gmail.com>
2024-01-09 23:52:41 -0300

    removing wrong import

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,7 +1,6 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,7 @@ import med.voll.api.paciente.PacienteRepository;
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
-    
+
     @Autowired
     private PacienteRepository repository;
 
