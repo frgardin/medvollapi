52de0c8ddf4b49d8e9ed40fea8e63dc362c7ea56
Felipe Gardin <gardinfelipe@gmail.com>
2024-01-10 22:26:18 -0300

    Page and size parameters addition in MedicoController class

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import med.voll.api.medico.DadosListagemMedico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.transaction.Transactional;
@@ -29,7 +30,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao){
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
         return repository
                 .findAll(paginacao)
                 .map(DadosListagemMedico::new);
