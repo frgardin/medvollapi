41362e5ef61bd1853b9ea56af8b5bf2e68d90e9f
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-01-10 22:35:22 -0300

    implement get method to PacienteController

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,12 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.paciente.DadosListagemPaciente;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -23,6 +25,13 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listagemPacientes (@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable){
+            return repository
+                    .findAll(pageable)
+                    .map(DadosListagemPaciente::new);
     }
     
 }
