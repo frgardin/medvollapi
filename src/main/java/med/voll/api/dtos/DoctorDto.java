dc152adb525a35373722781a7237c4fe78871d28
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-02-22 20:58:36 -0300

    formatting

--- src/main/java/med/voll/api/dtos/DoctorDto.java
+++ src/main/java/med/voll/api/dtos/DoctorDto.java
@@ -1,9 +1,7 @@
 package med.voll.api.dtos;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import med.voll.api.enums.ExpertiseEnum;
-
 
 public record DoctorDto(
         String name,
