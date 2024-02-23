d0ed49422a92bf85e3f697073836e8ab2ad2bb28
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-02-22 21:54:52 -0300

    adding patients and doctors tables

--- src/main/resources/db/migration/V2__create-table-patients.sql
+++ src/main/resources/db/migration/V2__create-table-patients.sql
@@ -1,10 +1,10 @@
-create table doctors(
+create table patients(
 
     id bigint not null auto_increment,
     name varchar(100) not null,
     email varchar(100) not null unique,
-    crm varchar(6) not null unique,
-    expertise varchar(100) not null,
+    cpf varchar(30) not null unique,
+    telephone varchar(100) not null unique,
     street varchar(100) not null,
     district varchar(100) not null,
     cep varchar(9) not null,
