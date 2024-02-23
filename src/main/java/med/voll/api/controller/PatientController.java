846f6d6f775730aed0e73ae1296fc3f20df45a90
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-02-23 00:00:31 -0300

    creating get pageable for patient

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -1,14 +1,15 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.Patient;
 import med.voll.api.domain.PatientRepository;
 import med.voll.api.dtos.PatientDto;
+import med.voll.api.dtos.PatientListResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/patients")
@@ -17,7 +18,13 @@ public class PatientController {
     @Autowired
     private PatientRepository repository;
 
+    @GetMapping
+    public Page<PatientListResponse> getPatientsPaginated (Pageable pageable) {
+        return repository.findAll(pageable).map(PatientListResponse::new);
+    }
+
     @PostMapping
+    @Transactional
     public void postPatient(@RequestBody @Valid PatientDto patientDto) {
         repository.save(new Patient(patientDto));
     }
--- src/main/java/med/voll/api/dtos/PatientListResponse.java
+++ src/main/java/med/voll/api/dtos/PatientListResponse.java
@@ -1,4 +1,20 @@
 package med.voll.api.dtos;
 
-public record PatientListReponse() {
+import med.voll.api.domain.Patient;
+
+public record PatientListResponse(
+        String name,
+        String email,
+        String telephone,
+        String cpf
+) {
+
+    public PatientListResponse(Patient patient) {
+        this(
+                patient.getName(),
+                patient.getEmail(),
+                patient.getTelephone(),
+                patient.getCpf()
+        );
+    }
 }
